25a358f39c42ee7a5c692a0728690f8f5895d526
Fatoumata <fahtima8778@gmail.com>
2019-06-12 14:45:22 +0800

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -8,30 +8,16 @@ app = Flask(__name__)
 #HOST = 'https://dry-lowlands-71228.herokuapp.com/'
 HOST = 'https://04f2e852.ngrok.io'
 
-TOKEN = '603627825:AAGMCcwk2vLI5VyOLvsBJ46vPUOSvKxD2ng'
+TOKEN = 'bot token here!'
 bot = telegram.Bot(token=TOKEN)
 botName = 'Fatoumata'
 
 ''' mLAB setting '''
 import pymongo
 from pymongo import MongoClient
-mongo = MongoClient('mongodb://Fahtima:Bot123@ds157256.mlab.com:57256/teleg_bot')
-db = mongo['teleg_bot']
+mongo = MongoClient('mongodb://Fahtima:Bot123@ds157256.mlab.com:57256/dbname')
+db = mongo['dbname']
 ''' End of mLAB setting '''
-
-def create_profile():
-    profile  = {
-        "Name": "Leila",
-        "Phone": ["982833049043",  "09293048u52843", "qw548293854"],
-        "Address": {
-            "postbox": 50505,
-            "house No": 34,
-            "city": "Wilayah Kuala Lumpur",
-            "country": "Malaysia"
-        }
-     }
-    pro.insert_one(profile)
-    print ("done !!!" )
 
 from modules.telegramBot.main import main
 
